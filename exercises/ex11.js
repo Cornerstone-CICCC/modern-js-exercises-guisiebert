b38e04aeef0c82474af65ed442386331112b1bdb
Guilherme Siebert <guilhermebsiebert@gmail.com>
2026-01-30 10:40:22 -0800

    solutions for ex11

--- exercises/ex11.js
+++ exercises/ex11.js
@@ -28,25 +28,37 @@ Implement the functions one by one. The example inputs and outputs below will he
 const PI = 3.14159;
 
 const sphereVolume = function (radius) {
-  // Code here!
+  return 4/3 * PI * radius ** 3
 };
 
 console.log(4186 < sphereVolume(10) && sphereVolume(10) < 4189); //true
 
 const coneVolume = function (radius, height) {
-  // And here!
+  return PI * radius ** 2 * height/3
 };
 
 console.log(45 < coneVolume(3, 5) && coneVolume(3, 5) < 49); //true
 
 const prismVolume = function (height, width, depth) {
-  // Probably here too!
+  return height * width * depth
 };
 
 console.log(prismVolume(3, 4, 5) === 60); //true
 
 const totalVolume = function (solids) {
-  // Code here? Yup!
+  let volumeSum = 0
+
+  for (let solid of solids) {
+    if (solid.type === "sphere") {
+      volumeSum = volumeSum + sphereVolume(solid.radius)
+    } else if (solid.type === "cone") {
+      volumeSum = volumeSum + coneVolume(solid.radius, solid.height)
+    } else {
+      volumeSum = volumeSum + prismVolume(solid.height, solid.width, solid.depth)
+    }
+  }
+
+  return volumeSum
 };
 
 const largeSphere = {
